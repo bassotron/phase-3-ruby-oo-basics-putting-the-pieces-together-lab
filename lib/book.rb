81a665e2aba5a386a6c161834bea2dd739c83e9f
Will Lovell <willard.lovell@icloud.com>
2022-09-07 13:00:37 -0600

    commit tree 13acdac429be867eeeea3d205f49e2cb720cdd6e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+class Book
+
+	attr_accessor :title
+	attr_accessor :author
+	attr_accessor :page_count
+	attr_accessor :genre
+	attr_accessor :turn_page
+
+def initialize(title)
+@title = title
+end
+
+def author=(author)
+	@author = author
+end
+
+
+
+def turn_page
+	puts "Flipping the page...wow, you read fast!"
+end
+
+
+
+
+
+
+
+
+
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,44 @@
-# Make your shoe class here!
+class Shoe 
+
+attr_accessor :brand
+attr_accessor :properties
+attr_accessor :color
+attr_accessor :size
+attr_accessor :material
+
+def initialize(brand)
+	@brand = brand
+end
+
+def brand=(brand)
+end
+def color=(color)
+	@color = color
+end
+def size=(size)
+	@size = size
+end
+def material=(material)
+	@material = material
+end
+
+
+def condition=(condition)
+@condition = condition
+end
+
+def condition
+@condition
+end
+
+
+def cobble
+	@condition = "new"
+	puts "Your shoe is as good as new!"
+	
+
+end
+
+
+
+end
